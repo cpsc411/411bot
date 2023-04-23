9631ece4139cca3442b7d53a007d57c5dc00862f
Markus de Medeiros <markusde@student.ubc.ca>
2023-04-22 23:36:25 -0700

    implement it

--- src/commands/eval.ts
+++ src/commands/eval.ts
@@ -3,73 +3,128 @@ import {Client, Message} from "discord.js";
 
 // modified from repeat
 
-var parse = require("s-expression");
+// 411-values-lang-v420
+// bug bounty program:
+// if you see a bug throw your entire computer in the dumpster
 
-const bound_vars = (args: any): any => {
+import parse = require("s-expression");
+
+// fixme: do capture avoiding substitution dumbass
+
+const do_subst = (args: any, x: string, a1: any): any => {
 	if (typeof args === "string") {
-		return [];
-	} else if (args[0] === "lambda") {
-		return bound_vars(args[2]).concat([args[1]]);
-	} else {
-		return bound_vars(args[2]).concat(bound_vars([args[1]]));
-	}
-};
-
-// freshen all disallowed free variables in an expression
-const freshen_against = (expr: any, disallowed: any, subst: any): any => {
-	if (typeof expr === "string") {
-		return subst(expr);
-	} else if (expr[0] === "lambda") {
-		if (disallowed.includes(expr[1])) {
-			return "unimplemented"; //-----------------
+		if (args == x) {
+			return a1;
 		} else {
-			return "unimplemented"; //-----------------
+			return args;
 		}
+	} else if (args[0] == "lambda") {
+		if (args[1] == x) {
+			return args;
+		} else {
+			return ["lambda", args[1], do_subst(args[2], x, a1)];
+		}
+	} else if (Array.isArray(args)) {
+		return args.map((ei) => do_subst(ei, x, a1));
 	} else {
-		return bound_vars(expr[2]).concat(bound_vars([expr[1]]));
+		throw "shid";
 	}
 };
 
-const reduce_step = (args: any): any => {
+const reduce_step = (args: any): [boolean, any] => {
 	// if at a redex, reduce it.
-	// if (Array.isArray(args)
-	//       && Array.length )
-	//   {
-	//       return args;
-	//   }
-	return args;
+	if (Array.isArray(args) && args[0] !== "lambda" && Array.isArray(args[0]) && args[0][0] === "lambda") {
+		return [true, [do_subst(args[0].slice(2), args[0][1], args[1])].concat(args.slice(2))];
+	}
+
+	// Otherwise look for a redex.
+	if (typeof args === "string") {
+		return [false, args];
+	} else if (args[0] == "lambda") {
+		for (let i = 2; i < args.length; i++) {
+			const res = reduce_step(args[i]);
+			if (res[0]) {
+				return [
+					true,
+					args
+						.slice(0, i)
+						.concat([res[1]])
+						.concat(args.slice(i + 1)),
+				];
+			}
+		}
+		return [false, args];
+	} else if (Array.isArray(args)) {
+		// applications are left associative
+		// so any redexes are only a subterm of a app term
+		for (let i = 0; i < args.length; i++) {
+			const res = reduce_step(args[i]);
+			if (res[0]) {
+				return [
+					true,
+					args
+						.slice(0, i)
+						.concat([res[1]])
+						.concat(args.slice(i + 1)),
+				];
+			}
+		}
+	}
+	return [false, args];
 };
 
 // rewrite (or check) input data is in the right format
-// NO syntax checking is for the birds
-// const to_ast = (args: any): any => {
-//     if (typeof(args) === 'string' && (args !== "lambda")) {
-//         return args;
-//     } else if (Array.isArray(args) && (args.length == 1)) {
-//         // one argument
-//         // remove extra brackets around statements
-//         return to_ast(args[0]);
-//     } else if (Array.isArray(args) && (args.length == 2)) {
-//         // two arguments
-//         return [to_ast(args[0]), to_ast(args[1])];
-//     } else if (Array.isArray(args) && (args.length >= 3) && (args[0] === "lambda") && (typeof(args[1]) === 'string') && (args[1] !== "lambda")) {
-//         // 3 arguments, lambda expression
-//         return ["lambda", args[1], to_ast(args.slice(2))];
-//     }  else if (Array.isArray(args) && (args.length >= 3)) {
-//         // >= 3 arguments, rewrite to be explicitly left-associative
-//         return [to_ast(args.slice(0, args.length - 1)), to_ast(args[args.length - 1]),];
-//     } else {
-//         throw "bad syntax";
-//     }
-// }
+const to_ast = (args: any): any => {
+	if (typeof args === "string" && args !== "lambda") {
+		return args;
+	} else if (Array.isArray(args) && args.length == 1) {
+		// one argument
+		// remove extra brackets around statements
+		return to_ast(args[0]);
+	} else if (Array.isArray(args) && args.length == 2) {
+		// two arguments
+		return [to_ast(args[0]), to_ast(args[1])];
+	} else if (
+		Array.isArray(args) &&
+		args.length >= 3 &&
+		args[0] === "lambda" &&
+		typeof args[1] === "string" &&
+		args[1] !== "lambda"
+	) {
+		// at least 3 arguments, lambda expression
+		return ["lambda", args[1], to_ast(args.slice(2))];
+	} else if (Array.isArray(args) && args.length >= 3) {
+		// An implicitly left-associative application
+		return args.map(to_ast);
+	} else {
+		throw "bad syntax";
+	}
+};
 
 const unparse = (args: any): string => {
+	const error_str = "oh noes i dwopped my awway! pwease fowgive me ~~";
+	const unparse_body = (args: any): string => {
+		if (typeof args === "string") {
+			return args;
+		} else if (Array.isArray(args) && args.length == 1) {
+			return unparse(args[0]);
+		} else if (Array.isArray(args)) {
+			return `(${args.map(unparse_body).join(" ")})`;
+		} else {
+			return error_str;
+		}
+	};
+
 	if (typeof args === "string") {
 		return args;
+	} else if (args[0] == "lambda") {
+		return `(${args.map(unparse_body).join(" ")})`;
+	} else if (Array.isArray(args) && args.length == 1) {
+		return unparse(args[0]);
 	} else if (Array.isArray(args)) {
-		return `(${args.map(unparse).join(" ")})`;
+		return `${args.map(unparse_body).join(" ")}`;
 	} else {
-		return "oh noes i dwopped my awway! pwease fowgive me ~~";
+		return error_str;
 	}
 };
 
@@ -79,14 +134,20 @@ const eval_lc = (args: string[]): string => {
 		// its basically just a galois connection
 		//  * - reduce (lambda calculus) -> *
 		//  |                               ^
-		//  parse . to_ast                  |
+		//  parse                           |
 		//  |                            unparse
 		//  v                               |
 		//  * -------- reduce_step -------> *
 		//  and the rest of the implementation follows from that
-		return unparse(reduce_step(to_ast(parse(`(${args.join(" ")})`))));
+		const rv = reduce_step(to_ast(parse(`(${args.join(" ")})`)));
+		if (rv[0]) {
+			// If there's still work to do, return a command that will evaluate the next step
+			return `eval! ${unparse(rv[1])}`;
+		}
 		// If there's no change, return a pretty printed result
-		// If there's still work to do, return a command that will evaluate the next step
+		else {
+			return unparse(rv[1]);
+		}
 	} catch (err) {
 		// literally just catch whatever other errors happen and give no feedback
 		// (autograder moment xd)
